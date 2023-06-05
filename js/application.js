5d3c80a38624bece06987809e31ef4a16f464613
Jordan <j.luse7@gmail.com>
2023-06-04 20:39:52 -0700

    Tallied up card totals

---
+++ js/application.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  let cartTotal = 0
+  $('tbody tr').each(function (i, ele) {
+    let cardQuantity = parseFloat($(ele).children('.quantity').text())
+    let carAskingPrice = parseFloat($(ele).children('.asking-price').text())
+    let totalAmountForCards = cardQuantity * carAskingPrice
+    $(ele).children('.cards-total').html(totalAmountForCards)
+    cartTotal += totalAmountForCards
+  });
+
+  $('.grand-total').append(cartTotal)
+});
