5f150c457214d8ffc1b49500bdb12c7771fd4ded
Jordan <j.luse7@gmail.com>
2023-06-07 18:34:28 -0700

    Pushing pokecart for desktop usage

--- js/application.js
+++ js/application.js
@@ -1,15 +1,38 @@
-$(document).ready(function () {
+let updatePokeCart = function (ele) {
+  // let cardTotal = 0
+  let cardQuantity = parseFloat($(ele).children('.quantity').text())
+  let carAskingPrice = parseFloat($(ele).children('.asking-price').text())
+  let totalAmountForCards = cardQuantity * carAskingPrice
+
+  $(ele).children('.cards-total').html(totalAmountForCards)
+
+  // cartTotal += totalAmountForCards
+  return totalAmountForCards;
+}
+
+let updateGrandTotal = function(totalForCards) {
   let cartTotal = 0
+  cartTotal += totalForCards
+  $('#grand-total').append(cartTotal)
+}
+
+$(document).ready(function () {
+  // let cartTotal = 0
+  // $('tbody tr').each(function (i, ele) {
+  //   let cardQuantity = parseFloat($(ele).children('.quantity').text())
+  //   let carAskingPrice = parseFloat($(ele).children('.asking-price').text())
+  //   let totalAmountForCards = cardQuantity * carAskingPrice
+  //   $(ele).children('.cards-total').html(totalAmountForCards)
+  //   cartTotal += totalAmountForCards
+  // });
+
   $('tbody tr').each(function (i, ele) {
-    let cardQuantity = parseFloat($(ele).children('.quantity').text())
-    let carAskingPrice = parseFloat($(ele).children('.asking-price').text())
-    let totalAmountForCards = cardQuantity * carAskingPrice
-    $(ele).children('.cards-total').html(totalAmountForCards)
-    cartTotal += totalAmountForCards
+    updatePokeCart(ele)
+    updateGrandTotal(updatePokeCart(ele))
   });
 
-  $('#grand-total').append(cartTotal)
-  $('.remove-btn').on('click', function() {
-    $(this).closest('tr').remove();
-  })
+  // $('#grand-total').append(cartTotal)
+  // $('.remove-btn').on('click', function() {
+  //   $(this).closest('tr').remove();
+  // })
 });
