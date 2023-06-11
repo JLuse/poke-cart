1b3aa55add6836b5425f7e344e43b99c35efb29c
Jordan <j.luse7@gmail.com>
2023-06-11 16:18:30 -0700

    Finished MVP, now styling

--- js/application.js
+++ js/application.js
@@ -1,9 +1,8 @@
 let updatePokeCart = function (ele) {
-  let cardQuantity = parseFloat($(ele).find('.quantity input').val())
-  console.log(cardQuantity)
-  let cardAskingPrice = parseFloat($(ele).children('.asking-price').text())
+  let cardQuantity = Number($(ele).find('.quantity input').val())
+  let cardAskingPrice = Number($(ele).children('.asking-price').children('.dollar-price').text())
   let totalAmountForCards = cardQuantity * cardAskingPrice
-  $(ele).children('.cards-total').html(totalAmountForCards)
+  $(ele).children('.cards-total').html('$' + totalAmountForCards)
   return totalAmountForCards;
 }
 
@@ -16,8 +15,7 @@ let getGrandTotal = function() {
   let pokeCartTotal = pokecartCardValues.reduce((acc, currentVal) => {
     return acc + currentVal
   })
-  // need to fix
-  $('#grand-total').append(pokeCartTotal)
+  $('#grand-total').html(pokeCartTotal)
 }
 
 
@@ -29,7 +27,49 @@ $(document).ready(function () {
     getGrandTotal()
   })
 
-  $('tr input').on('input', function () {
-    getGrandTotal()
+  var timeout;
+  $(document).on('input', 'tr input', function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+      getGrandTotal()
+    }, 200);
   });
-});
+
+  $('#addCard').on('submit', function(e) {
+    e.preventDefault();
+    let pokemon = $(this).children('[name=pokemon]').val()
+    let cardType = $(this).children('[name=type]').val()
+    let askingPrice = $(this).children('[name=asking-price]').val()
+    let quantity = $(this).children('[name=quantity]').val()
+
+  //   $('tbody').append('<tr>' +
+  //   '<td class="pokemon-name">' + pokemon + '</td>' +
+  //   '<td class="card-type">' + cardType + '</td>' +
+  //   '<td class="asking-price">$<span class="dollar-price">' + askingPrice + '<span/></td>' +
+  //   '<td class="quantity"><input type="number" value="' + quantity + '" /></td>' +
+  //   '<td class="marketValue"></td>' +
+  //   '<td class="profit"></td>' +
+  //   '<td><button class="btn btn-light btn-sm remove">remove</button></td>' +
+  // '</tr>');
+  $('tbody').append(
+    `<tr>
+      <td class="pokemon-name">${pokemon}</td>
+      <td class="card-type">${cardType}</td>
+      <td class="asking-price">$<span class="dollar-price">${askingPrice}</span></td>
+      <td class="quantity"><input type="number" value="${!quantity ? 0 : quantity}"/></td>
+      <td class="cards-total"></td>
+      <td>
+        <button class="btn btn-danger btn-sm remove-btn">Remove</button>
+      </td>
+    </tr>
+  `)
+
+  $(this).children('[name=pokemon]').val()
+  $(this).children('[name=type]').val()
+  $(this).children('[name=asking-price]').val()
+  $(this).children('[name=quantity]').val()
+
+  getGrandTotal()
+  });
+
+}); // end
